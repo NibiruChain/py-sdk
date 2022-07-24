5f65b3786a059163d812034b2a496dee535abd75
NibiruHeisenberg <101130700+NibiruHeisenberg@users.noreply.github.com>
2022-07-24 15:38:28 -0700

    Fix proto-gen script
    It was creating an unnecessary folder under nibiru/

--- scripts/protocgen.sh
+++ scripts/protocgen.sh
@@ -14,17 +14,21 @@ protoc_gen_gocosmos() {
   go get github.com/cosmos/cosmos-sdk 2>/dev/null
 }
 
+# grab the cosmos-sdk proto file locations from disk
 cd ../nibiru;
 protoc_gen_gocosmos
-
 cosmos_sdk_dir=$(go list -f '{{ .Dir }}' -m github.com/cosmos/cosmos-sdk)
+
+# grab all of the proto directories
 cd -;
+mkdir nibiru/proto/
 proto_dirs=$(find $cosmos_sdk_dir/proto $cosmos_sdk_dir/third_party/proto ./proto -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
 echo $proto_dirs
+
+# generate the protos for each directory
 for dir in $proto_dirs; do \
   echo "generating $dir"
-  echo "$cosmos_sdk_dir"
-  mkdir -p ./nibiru/${dir}; \
+  # echo "$cosmos_sdk_dir"
   python3 -m grpc_tools.protoc \
     -I proto \
     -I "$cosmos_sdk_dir/third_party/proto" \
@@ -35,5 +39,8 @@ for dir in $proto_dirs; do \
     --mypy_grpc_out=nibiru/proto \
     $(find "${dir}" -type f -name '*.proto')
 done; \
-rm -rf proto
+
 printf "import os\nimport sys\n\nsys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))" > nibiru/proto/__init__.py
+
+# cleanup
+rm -rf proto/

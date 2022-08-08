67745c94fc963223147723ce1fdd1bdca5672ee3
matthiasmatt <md2022@matrixsystems.co>
2022-08-08 18:21:39 -0300

    feat: improve output of txs and logging

--- nibiru/client.py
+++ nibiru/client.py
@@ -1,27 +1,26 @@
+import json
 from typing import List, Optional, Tuple, Union
 
 import grpc
 
-from .clients import Dex as DexClient
-from .clients import Perp as PerpClient
-from .clients import Pricefeed as PricefeedClient
-from .clients import VPool as VPoolClient
-from .exceptions import NotFoundError
-from .network import Network
-from .proto.cosmos.auth.v1beta1 import auth_pb2 as auth_type
-from .proto.cosmos.auth.v1beta1 import query_pb2 as auth_query
-from .proto.cosmos.auth.v1beta1 import query_pb2_grpc as auth_query_grpc
-from .proto.cosmos.authz.v1beta1 import query_pb2 as authz_query
-from .proto.cosmos.authz.v1beta1 import query_pb2_grpc as authz_query_grpc
-from .proto.cosmos.bank.v1beta1 import query_pb2 as bank_query
-from .proto.cosmos.bank.v1beta1 import query_pb2_grpc as bank_query_grpc
-from .proto.cosmos.base.abci.v1beta1 import abci_pb2 as abci_type
-from .proto.cosmos.base.tendermint.v1beta1 import query_pb2 as tendermint_query
-from .proto.cosmos.base.tendermint.v1beta1 import (
-    query_pb2_grpc as tendermint_query_grpc,
-)
-from .proto.cosmos.tx.v1beta1 import service_pb2 as tx_service
-from .proto.cosmos.tx.v1beta1 import service_pb2_grpc as tx_service_grpc
+from nibiru.clients import Dex as DexClient
+from nibiru.clients import Perp as PerpClient
+from nibiru.clients import Pricefeed as PricefeedClient
+from nibiru.clients import VPool as VPoolClient
+from nibiru.exceptions import NotFoundError
+from nibiru.network import Network
+from nibiru.proto.cosmos.auth.v1beta1 import auth_pb2 as auth_type
+from nibiru.proto.cosmos.auth.v1beta1 import query_pb2 as auth_query
+from nibiru.proto.cosmos.auth.v1beta1 import query_pb2_grpc as auth_query_grpc
+from nibiru.proto.cosmos.authz.v1beta1 import query_pb2 as authz_query
+from nibiru.proto.cosmos.authz.v1beta1 import query_pb2_grpc as authz_query_grpc
+from nibiru.proto.cosmos.bank.v1beta1 import query_pb2 as bank_query
+from nibiru.proto.cosmos.bank.v1beta1 import query_pb2_grpc as bank_query_grpc
+from nibiru.proto.cosmos.base.abci.v1beta1 import abci_pb2 as abci_type
+from nibiru.proto.cosmos.base.tendermint.v1beta1 import query_pb2 as tendermint_query
+from nibiru.proto.cosmos.base.tendermint.v1beta1 import query_pb2_grpc as tendermint_query_grpc
+from nibiru.proto.cosmos.tx.v1beta1 import service_pb2 as tx_service
+from nibiru.proto.cosmos.tx.v1beta1 import service_pb2_grpc as tx_service_grpc
 
 DEFAULT_TIMEOUTHEIGHT = 20  # blocks
 
@@ -110,7 +109,7 @@ class Client:
             req = tx_service.SimulateRequest(tx_bytes=tx_byte)
             return self.stubTx.Simulate.__call__(req), True
         except grpc.RpcError as err:
-            return err, False
+            return json.loads(err._state.debug_error_string)["grpc_message"], False
 
     def send_tx_sync_mode(self, tx_byte: bytes) -> abci_type.TxResponse:
         req = tx_service.BroadcastTxRequest(tx_bytes=tx_byte, mode=tx_service.BroadcastMode.BROADCAST_MODE_SYNC)
--- tests/chain_info_test.py
+++ tests/chain_info_test.py
@@ -7,6 +7,7 @@ import nibiru
 from nibiru import common
 from typing import Any, Dict, Union, List
 
+
 @dataclasses.dataclass
 class TestConfig:
     LCD_PORT = "1317"
@@ -15,10 +16,12 @@ class TestConfig:
     CHAIN_ID = os.environ["CHAIN_ID"]
     HOST = os.environ["HOST"]
 
+
 CONFIG = TestConfig()
 
+
 def test_genesis_block_ping():
-    """Manually query block info from the chain using a get request. This verifies that 
+    """Manually query block info from the chain using a get request. This verifies that
     the configuration is valid.
     """
     host = "34.130.24.87"
@@ -27,6 +30,7 @@ def test_genesis_block_ping():
     url = f"http://{host}:{port}/block?height={block_number}"
     query_resp: Dict[str, Any] = requests.get(url).json()
     assert all([key in query_resp.keys() for key in ["jsonrpc", "id", "result"]])
+
 
 @pytest.fixture
 def network() -> nibiru.Network:
@@ -39,16 +43,18 @@ def network() -> nibiru.Network:
         env="local",
     )
 
+
 @pytest.fixture
 def val_node(network: nibiru.Network) -> nibiru.Sdk:
     tx_config = nibiru.TxConfig(tx_type=common.TxType.BLOCK)
     newtork_insecure: bool = True
-    return nibiru.Sdk.authorize(CONFIG.VALIDATOR_MNEMONIC).with_config(
-        tx_config).with_network(network, newtork_insecure)
+    return (
+        nibiru.Sdk.authorize(CONFIG.VALIDATOR_MNEMONIC).with_config(tx_config).with_network(network, newtork_insecure)
+    )
+
 
 def test_get_chain_id(val_node: nibiru.Sdk):
     assert CONFIG.CHAIN_ID == val_node.query.get_chain_id()
-
 
 
 def test_query_perp_params(val_node: nibiru.Sdk):
@@ -59,9 +65,7 @@ def test_query_perp_params(val_node: nibiru.Sdk):
         "epochIdentifier",
         "feePoolFeeRatio",
         "liquidationFeeRatio",
-        "maintenanceMarginRatio",
         "partialLiquidationRatio",
         "twapLookbackWindow",
     ]
     assert all([(param_name in params) for param_name in perp_param_names])
-

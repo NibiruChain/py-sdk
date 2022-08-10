7a4cb60463411a2341bba263bd28cc7283321884
matthiasmatt <md2022@matrixsystems.co>
2022-08-10 18:47:35 -0300

    fix: Bump version to 0.0.13

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ URL = "https://github.com/NibiruChain/nibiru-py"
 EMAIL = "dev@nibiru.fi"
 AUTHOR = "Nibiru Chain"
 REQUIRES_PYTHON = ">=3.9.0"
-VERSION = "0.0.12"
+VERSION = "0.0.13"
 
 REQUIRED = [
     "grpcio",

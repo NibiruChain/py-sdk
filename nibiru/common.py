a3e8af64b44f4a774a7afaac0b9711a7d6b20a8d
Mat-Cosmos <97468149+matthiasmatt@users.noreply.github.com>
2022-08-15 11:49:13 -0300

    Add sdk doc (network, txconfig and sdk) (#69)
    * doc: Add doc to the sdk module
    
    * doc: Add doc to the network module
    
    * doc: Add doc TxConfig and TxType
    
    * doc: typo
    
    * fix: make sure we don't treat none as string

--- nibiru/common.py
+++ nibiru/common.py
@@ -5,9 +5,34 @@ from .proto.cosmos.base.v1beta1 import coin_pb2 as cosmos_base_coin_pb
 
 
 class TxType(Enum):
+    """
+    The TxType allows you to chose what type of synchronization you want to use to send transaction
+    """
+
     SYNC = 1
+    """
+    The CLI waits for a CheckTx execution response only.
+    Each full-node that receives a transaction sends a CheckTx to the application layer to check for validity, and
+    receives an abci.ResponseCheckTx. If the Tx passes the checks, it is held in the nodes' Mempool , an in-memory pool
+    of transactions unique to each node pending inclusion in a block - honest nodes will discard Tx if it is found to
+    be invalid.
+
+    Prior to consensus, nodes continuously check incoming transactions and gossip them to their peers.
+    """
+
     ASYNC = 2
+    """
+    The CLI returns immediately (transaction might fail silently).
+    If you send a transaction with this option, it is recommended to query the transaction output using the hash of the
+    transaction given by the output of the tx call.
+    """
+
     BLOCK = 3
+    """
+    The tx function will wait unitl the tx is be committed in a block.
+    This have the effect of having the full log of the transaction available with the output of the method. These logs
+    will include information as to coin sent and received, states changed etc.
+    """
 
 
 class Side(Enum):
@@ -37,16 +62,23 @@ class PoolAsset:
 
 class TxConfig:
     def __init__(
-        self, gas_wanted: int = 0, gas_multiplier: float = 0, gas_price: float = 0, tx_type: TxType = TxType.ASYNC
+        self,
+        gas_wanted: int = 0,
+        gas_multiplier: float = 1.25,
+        gas_price: float = 0,
+        tx_type: TxType = TxType.ASYNC,
     ):
-        '''
-        Parameters:
-            gas_wanted (int): Set the absolute gas_wanted to be used
-            gas_multiplier (float): Set the gas multiplier that's being applied to the estimated gas, defaults to 1.25.
-                                    If gas_wanted is set this property is ignored.
-            gas_price(float): Set the gas price used to calculate the fee
-            tx_type (TxType): Configure how to execute the tx, defaults to `ASYNC`
-        '''
+        """
+        The TxConfig object allows to customize the behavior of the Sdk interface when a transaction is sent.
+
+        Args:
+            gas_wanted (int, optional): Set the absolute gas_wanted to be used. Defaults to 0.
+            gas_multiplier (float, optional): Set the gas multiplier that's being applied to the estimated gas.
+                Defaults to 0. If gas_wanted is set this property is ignored.
+            gas_price (float, optional): Set the gas price used to calculate the fee. Defaults to 0.
+            tx_type (TxType, optional): Configure how to execute the tx. Defaults to TxType.ASYNC.
+        """
+
         self.gas_multiplier = gas_multiplier
         self.gas_wanted = gas_wanted
         self.gas_price = gas_price
--- nibiru/sdk.py
+++ nibiru/sdk.py
@@ -1,23 +1,43 @@
+"""
+The sdk is the main interface to the chain. Each sdk object needs to be authorized with a wallet, which can be generated
+with a new address if needed.
+
+Once instanciated, the sdk provide the sdk.tx and sdk.query modules to be able to query or send a transaction to the
+chain.
+
+This object depends on the network and transaction configuration the users want. These objects can be set using the
+Network and TxConfig classes respectively inside the nibiru/network.py and nibiru/common.py files.
+"""
 import logging
 
-from .client import GrpcClient
-from .common import TxConfig
-from .network import Network
-from .sdks.tx import TxClient
-from .wallet import PrivateKey
+from nibiru.client import GrpcClient
+from nibiru.common import TxConfig
+from nibiru.network import Network
+from nibiru.sdks.tx import TxClient
+from nibiru.wallet import PrivateKey
+
 
 class Sdk:
-    """TODO docs
-    
-    Example:
-    ```python
-    sdk = ( 
-        Sdk.authorize(val_mnemonic)
-        .with_config(tx_config)
-        .with_network(network, network_insecure) 
-    )
-    ```
     """
+    The Sdk class creates an interface to sign and send transactions or execute queries from a node.
+    It is associated to:
+        - a wallet, which can be either created or recovered from an existing mnemonic.
+        - a network, defining the node to connect to
+        - optionally a configuration defining how to behave and the gas configuration for each transaction
+
+    Each method starting with `with_` will replace the existing Sdk object with a new version having the defined
+    behavior.
+
+
+    Example ::
+
+        sdk = (
+            Sdk.authorize(val_mnemonic)
+            .with_config(tx_config)
+            .with_network(network, network_insecure)
+        )
+    """
+
     def __init__(self, _error_do_not_use_init_directly=None) -> None:
         """Unsupported, please use from_mnemonic to initialize."""
         if not _error_do_not_use_init_directly:
@@ -29,48 +49,98 @@ class Sdk:
         self.tx_config = TxConfig()
 
     @classmethod
-    def authorize(cls, key: str = "") -> "Sdk":
+    def authorize(cls, key: str = None) -> "Sdk":
+        """
+        Authorize allows the user to generate or recover a wallet and register it as an Sdk object.
+        If a key is provided, the wallet will be recovered. Otherwise, a new wallet is created.
+
+        Args:
+            key (str, optional): The mnemonic if recover needed. Defaults to None.
+
+        Returns:
+            Sdk: The updated sdk object
+        """
         self = cls(_error_do_not_use_init_directly=True)
-        if key == "":
+        if key is None:
             (mnemonic, pk) = PrivateKey.generate()
-            logging.warning("The mnemonic used for the newly generated key is: \n%s", mnemonic)
-            logging.warning("Please write down this key, it will NOT be recoverable otherwise")
-        if len(key.split(" ")) > 1:
+            logging.warning(
+                "The mnemonic used for the newly generated key is: \n%s", mnemonic
+            )
+            logging.warning(
+                "Please write down this key, it will NOT be recoverable otherwise"
+            )
+        elif len(key.split(" ")) > 1:
             pk = PrivateKey.from_mnemonic(key)
         elif len(key) > 0:
             pk = PrivateKey.from_hex(key)
 
-        self.with_priv_key(pk)
+        self._with_priv_key(pk)
 
         return self
 
     def with_network(self, network: Network, insecure=False) -> "Sdk":
+        """
+        Change the network of the sdk to the specified network.
+
+        Args:
+            network (Network): A network object
+            insecure (bool, optional): Wether the connection should be insecure or not. Defaults to False.
+
+        Returns:
+            Sdk: The updated sdk object
+        """
         self.network = network
-        self.with_query_client(GrpcClient(self.network, insecure))
-        return self
-
-    def with_query_client(self, client: GrpcClient) -> "Sdk":
-        self.query = client
-        tx_client = TxClient(client=self.query, network=self.network, priv_key=self.priv_key, config=self.tx_config)
-        self.with_tx_client(tx_client)
-        return self
-
-    def with_tx_client(self, tx_client: TxClient) -> "Sdk":
-        self.tx = tx_client
-        return self
-
-    def with_priv_key(self, priv_key: PrivateKey) -> "Sdk":
-        self.priv_key = priv_key
-        self.with_network(Network.local(), True)
+        self._with_query_client(GrpcClient(self.network, insecure))
         return self
 
     def with_config(self, config: TxConfig) -> "Sdk":
+        """
+        Change the configuration for trasnactions for the sdk to the specified config.
+
+        Args:
+            config (TxConfig): A transaction configuration object
+
+        Returns:
+            Sdk: The updated sdk object
+        """
         self.tx_config = config
-        tx_client = TxClient(client=self.query, network=self.network, priv_key=self.priv_key, config=self.tx_config)
-        self.with_tx_client(tx_client)
+        tx_client = TxClient(
+            client=self.query,
+            network=self.network,
+            priv_key=self.priv_key,
+            config=self.tx_config,
+        )
+        self._with_tx_client(tx_client)
         return self
 
     @property
     def address(self) -> str:
+        """
+        Returns the public address of the wallet.
+
+        Returns:
+            str: The public address of the wallet
+        """
         pub_key = self.priv_key.to_public_key()
         return pub_key.to_address().to_acc_bech32()
+
+    # Private methods
+    def _with_query_client(self, client: GrpcClient) -> "Sdk":
+        self.query = client
+        tx_client = TxClient(
+            client=self.query,
+            network=self.network,
+            priv_key=self.priv_key,
+            config=self.tx_config,
+        )
+        self._with_tx_client(tx_client)
+        return self
+
+    def _with_tx_client(self, tx_client: TxClient) -> "Sdk":
+        self.tx = tx_client
+        return self
+
+    def _with_priv_key(self, priv_key: PrivateKey) -> "Sdk":
+        self.priv_key = priv_key
+        self.with_network(Network.local(), True)
+        return self

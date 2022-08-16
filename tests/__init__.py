e1442fd84ee6a3072fe36e0b323566d97a11e5b4
Mat-Cosmos <97468149+matthiasmatt@users.noreply.github.com>
2022-08-16 19:12:35 -0300

    hotfix: fix the json parse for grpc error (#75)
    * hotfix: fix the json parse for grpc error
    
    * fix: Remove test file
    
    * test: add test for error handling
    
    * test(pricefeed): Add failing case and global test suite logger
    
    * test: Remove error
    
    * fix: linting
    
    Co-authored-by: Unique-Divine <realuniquedivine@gmail.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,28 @@
 """Tests for the nibiru package"""
+import logging
+import sys
+
+logging.basicConfig()
+
+
+def init_test_logger() -> logging.Logger:
+    test_logger = logging.getLogger("test-logger")
+    test_logger.setLevel(logging.DEBUG)
+
+    # Logs to stdout so we can at least see logs in GHA.
+    handler = logging.StreamHandler(sys.stdout)
+    handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter(
+        '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+    handler.setFormatter(formatter)
+    test_logger.addHandler(handler)
+    return test_logger
+
+
+LOGGER = init_test_logger()
+"""Simple logger to use throughout the test suite."""
 
 
 def dict_keys_must_match(dict_: dict, keys: list[str]):

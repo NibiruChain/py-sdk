66ebc18fbc3330d8cdde980a3bf7eaa8c439ce42
Mat-Cosmos <97468149+matthiasmatt@users.noreply.github.com>
2022-08-17 21:20:46 -0300

    Fix de-serialize issue with the latest proto files (#79)

--- nibiru/clients/util.py
+++ nibiru/clients/util.py
@@ -1,100 +1,50 @@
+from google.protobuf.json_format import MessageToDict
+
 from nibiru.utils import from_sdk_dec
 
-BASE_ATTRS = [
-    'ByteSize',
-    'Clear',
-    'ClearExtension',
-    'ClearField',
-    'CopyFrom',
-    'DESCRIPTOR',
-    'DiscardUnknownFields',
-    'Extensions',
-    'FindInitializationErrors',
-    'FromString',
-    'HasExtension',
-    'HasField',
-    'IsInitialized',
-    'ListFields',
-    'MergeFrom',
-    'MergeFromString',
-    'ParseFromString',
-    'RegisterExtension',
-    'SerializePartialToString',
-    'SerializeToString',
-    'SetInParent',
-    'UnknownFields',
-    'WhichOneof',
-    '_CheckCalledFromGeneratedFile',
-    '_SetListener',
-    '__class__',
-    '__deepcopy__',
-    '__delattr__',
-    '__dir__',
-    '__doc__',
-    '__eq__',
-    '__format__',
-    '__ge__',
-    '__getattribute__',
-    '__getstate__',
-    '__gt__',
-    '__hash__',
-    '__init__',
-    '__init_subclass__',
-    '__le__',
-    '__lt__',
-    '__module__',
-    '__ne__',
-    '__new__',
-    '__reduce__',
-    '__reduce_ex__',
-    '__repr__',
-    '__setattr__',
-    '__setstate__',
-    '__sizeof__',
-    '__slots__',
-    '__str__',
-    '__subclasshook__',
-    '__unicode__',
-    '_extensions_by_name',
-    '_extensions_by_number',
-]
+
+def camel_to_snake(s):
+    return ''.join(['_' + c.lower() if c.isupper() else c for c in s]).lstrip('_')
 
 
-def deserialize(obj: object) -> dict:
-    custom_dtypes = {
-        str(field[1]): field[0].GetOptions().__getstate__().get("serialized", None)
-        for field in obj.ListFields()
+def t_dict(d):
+    if isinstance(d, list):
+        return [t_dict(i) if isinstance(i, (dict, list)) else i for i in d]
+    return {
+        camel_to_snake(a): t_dict(b) if isinstance(b, (dict, list)) else b
+        for a, b in d.items()
     }
 
-    serialized_output = {}
 
-    for attr in dir(obj):
-        if attr not in BASE_ATTRS:
+def deserialize(proto_message: object) -> dict:
+    """
+    Take a proto message and convert it into a dictionnary.
+    sdk.Dec values are converted to be consistent with txs.
 
-            attr_search = obj.__getattribute__(attr)
+    Args:
+        proto_message (object): The proto message
 
-            custom_dtype = custom_dtypes.get(str(attr_search))
+    Returns:
+        dict: The dictionary
+    """
+    output = MessageToDict(proto_message)
 
-            if custom_dtype is not None:
+    is_sdk_dec = {
+        field.camelcase_name: "types.Dec" in str(field.GetOptions())
+        for field in proto_message.DESCRIPTOR.fields
+    }
 
-                if "cosmos/cosmos-sdk/types.Dec" in str(custom_dtype):
-                    serialized_output[str(attr)] = from_sdk_dec(
-                        obj.__getattribute__(attr)
-                    )
+    for field in proto_message.DESCRIPTOR.fields:
+        if field.message_type is not None:
+            # This is another proto object
+            try:
+                output[field.camelcase_name] = deserialize(
+                    proto_message.__getattribute__(field.camelcase_name)
+                )
+            except AttributeError:
+                output[field.camelcase_name] = output[field.camelcase_name]
 
-                else:
-                    try:
-                        val = obj.__getattribute__(attr)
-                        if hasattr(val, '__len__') and not isinstance(val, str):
-                            updated_vals = []
-                            for v in val:
-                                updated_vals.append(deserialize(v))
-                            serialized_output[str(attr)] = updated_vals
-                        else:
-                            serialized_output[str(attr)] = deserialize(val)
-                    except:
-                        serialized_output[str(attr)] = obj.__getattribute__(attr)
-            else:
-                serialized_output[str(attr)] = deserialize(obj.__getattribute__(attr))
+        elif is_sdk_dec[field.camelcase_name]:
+            output[field.camelcase_name] = from_sdk_dec(output[field.camelcase_name])
 
-    return serialized_output
+    return t_dict(output)

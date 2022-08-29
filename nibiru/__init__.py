8dee4f8b7cc12e26dc4a93cd08abbe920189b772
matthiasmatt <md2022@matrixsystems.co>
2022-08-29 10:35:57 -0300

    feat: cleanup for 0.3.1 release

--- nibiru/__init__.py
+++ nibiru/__init__.py
@@ -1,3 +1,7 @@
+import importlib.metadata as importlib_metadata
+
+__version__ = importlib_metadata.version(__name__)
+
 import google.protobuf.message
 
 ProtobufMessage = google.protobuf.message.Message

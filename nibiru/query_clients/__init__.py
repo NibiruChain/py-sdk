9ce34c79977fb71cc0d073c7cfaecab7198067e4
matthiasmatt <md2022@matrixsystems.co>
2022-09-05 15:53:14 -0300

    chore: cleanup

--- nibiru/query_clients/__init__.py
+++ nibiru/query_clients/__init__.py
@@ -1,5 +1,4 @@
 from nibiru.query_clients.dex import DexQueryClient  # noqa
 from nibiru.query_clients.perp import PerpQueryClient  # noqa
 from nibiru.query_clients.pricefeed import PricefeedQueryClient  # noqa
-from nibiru.query_clients.rpc_client import ChainQueryClient  # noqa
 from nibiru.query_clients.vpool import VpoolQueryClient  # noqa

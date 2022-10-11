e12c76a76f2f51a40291383b3962a2e2bd462753
Oleg Nikonychev <oleg.nikonychev@gmail.com>
2022-10-11 11:12:47 +0400

    Upgraded Nibiru Proto. Fixed auth test.

--- tests/auth_test.py
+++ tests/auth_test.py
@@ -5,7 +5,9 @@ import tests
 def test_query_auth_account(val_node: nibiru.Sdk):
     query_resp: dict = val_node.query.auth.account(val_node.address)["account"]
 
-    tests.dict_keys_must_match(query_resp, ['@type', 'address', 'pubKey', 'sequence'])
+    tests.dict_keys_must_match(
+        query_resp, ['@type', 'address', 'pubKey', 'sequence', 'accountNumber']
+    )
 
 
 def test_query_auth_accounts(val_node: nibiru.Sdk):
